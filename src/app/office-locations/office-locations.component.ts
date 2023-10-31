e936cff606540a855b201aac8e25a3a96c95cf0a
Brian Byrdsong <brian.byrdsong@gmail.com>
2023-10-30 22:40:10 -0400

    office location work

--- src/app/office-locations/office-locations.component.ts
+++ src/app/office-locations/office-locations.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../services/local-storage.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-office-locations',
   templateUrl: './office-locations.component.html',
-  styleUrls: ['./office-locations.component.css']
+  styleUrls: ['./office-locations.component.css'],
 })
-export class OfficeLocationsComponent {
+export class OfficeLocationsComponent implements OnInit {
+  fg: FormGroup;
+  officeList: any[] = [];
 
+  constructor(private fb: FormBuilder, private lss: LocalStorageService) {
+    this.fg = this.fb.group({
+      id: new FormControl(),
+      name: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      zip: new FormControl('', [Validators.required]),
+    });
+  }
+
+  ngOnInit(): void {
+    this.getOffices();
+  }
+
+  onSubmit() {
+    const office = this.fg.value;
+    this.lss.upsertOffice(office);
+    this.fg.reset();
+    this.getOffices();
+  }
+
+  getOffices() {
+    this.officeList = this.lss.getOffices();
+  }
 }
--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,35 +1,53 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+const ROLETABLE = 'roles';
+const OFFICETABLE = 'offices';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
   roleList: any[] = [];
-  constructor() {}
+  officeList: any[] = [];
 
+  getRoles() {
+    return JSON.parse(localStorage.getItem(ROLETABLE) || '[]');
+  }
   upsertRole(role: any) {
     if (role.id) {
       const index = this.roleList.findIndex((r) => r.id === role.id);
       this.roleList[index] = role;
     } else {
-      const newRole = this.checkAndSetId(role);
+      const newRole = this.checkAndSetId(role, this.roleList);
 
       this.roleList.push(newRole);
     }
-    localStorage.setItem('roles', JSON.stringify(this.roleList));
+    localStorage.setItem(ROLETABLE, JSON.stringify(this.roleList));
   }
 
-  checkAndSetId(role: any) {
-    role.id = Math.floor(Math.random() * 1000000);
+  getOffices() {
+    return JSON.parse(localStorage.getItem(OFFICETABLE) || '[]');
+  }
+  upsertOffice(office: any) {
+    if (office.id) {
+      const index = this.officeList.findIndex((r) => r.id === office.id);
+      this.roleList[index] = office;
+    } else {
+      const newOffice = this.checkAndSetId(office, this.officeList);
 
-    if (this.roleList.some((r) => r.id === role.id)) {
-      this.checkAndSetId(role);
+      this.officeList.push(newOffice);
+    }
+    localStorage.setItem(OFFICETABLE, JSON.stringify(this.officeList));
+  }
+
+  checkAndSetId(model: any, arr: any[]) {
+    model.id = Math.floor(Math.random() * 1000000);
+
+    if (arr.some((r) => r.id === model.id)) {
+      this.checkAndSetId(model, arr);
     }
 
-    return role;
-  }
-
-  getRoles() {
-    return JSON.parse(localStorage.getItem('roles') || '[]');
+    return model;
   }
 }
