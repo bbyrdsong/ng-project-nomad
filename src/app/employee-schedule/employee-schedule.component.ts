05ed417bd604f04ff4b6f98639e59ec1fa031f77
Brian Byrdsong <brian.byrdsong@gmail.com>
2023-11-06 17:46:17 -0500

    working schedule

--- src/app/employee-schedule/employee-schedule.component.ts
+++ src/app/employee-schedule/employee-schedule.component.ts
@@ -18,6 +18,7 @@ export class EmployeeScheduleComponent implements OnInit {
   employeeList: any[] = [];
   officeList: any[] = [];
   employeeRoleList: any[] = [];
+  calendar: any[] = [];
 
   constructor(private fb: FormBuilder, private lss: LocalStorageService) {
     this.fg = this.fb.group({
@@ -37,6 +38,8 @@ export class EmployeeScheduleComponent implements OnInit {
     this.employeeList = this.lss.getEmployees();
     this.officeList = this.lss.getOffices();
     this.employeeRoleList = this.lss.getRoles();
+
+    this.setCalendar(2023, 10);
   }
 
   getScheduleList() {
@@ -54,5 +57,38 @@ export class EmployeeScheduleComponent implements OnInit {
     const employee = this.employeeList.find((e) => e.id == id);
     this.fg.controls['employeeRoleId'].setValue(employee.employeeRoleId);
     this.fg.controls['officeLocationId'].setValue(employee.officeLocationId);
+  }
+
+  setCalendar(year: number, month: number) {
+    const nextMonth = month + 1 === 12 ? 0 : month + 1;
+    const currentDate = new Date(year, month, 1);
+    const lastDayOfMonth = new Date(
+      nextMonth == 0 ? year + 1 : year,
+      nextMonth,
+      0
+    );
+    let daysStarted = false;
+    for (let i = 0; i < 36; i++) {
+      if (!daysStarted) {
+        if (currentDate && currentDate.getDay() == i) {
+          this.calendar.push(currentDate.getDate());
+          currentDate.setDate(currentDate.getDate() + 1);
+          daysStarted = true;
+        } else {
+          this.calendar.push('');
+        }
+      } else {
+        if (currentDate > lastDayOfMonth) {
+          this.calendar.push('');
+          continue;
+        }
+        this.calendar.push(currentDate.getDate());
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    }
+    console.log(nextMonth);
+    console.log(currentDate);
+    console.log(lastDayOfMonth);
+    console.log(this.calendar);
   }
 }
